2e38c793755f73db6d23b31fda87a5dc035c9da4
sinhaugoh <sinhaugoh456@hotmail.com>
2022-08-06 22:48:57 +0800

    created ItemPostFactory for testing

--- core/model_factories.py
+++ core/model_factories.py
@@ -9,7 +9,20 @@ class UserFactory(factory.django.DjangoModelFactory):
     password = factory.PostGenerationMethodCall('set_password', 'ShareGarden')
     profile_image = factory.django.ImageField()
     about = 'this is about section'
-    address = 'Yew tee Mrt'
 
     class Meta:
         model = User
+
+
+class ItemPostFactory(factory.django.DjangoModelFactory):
+    title = factory.Sequence(lambda n: 'item{}'.format(n))
+    quantity = 1
+    pick_up_information = 'This is pick up information'
+    location = 'Yew tee mrt'
+    category = ItemPost.Category.GIVEAWAY
+    item_type = ItemPost.ItemType.SEED_OR_PLANT
+    cover_image = factory.django.ImageField()
+    created_by = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = ItemPost
