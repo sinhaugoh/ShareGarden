90f6c89a3665842db4de88fd2c539242e3187acb
sinhaugoh <sinhaugoh456@hotmail.com>
2022-08-24 15:30:25 +0800

    removed unnecessary import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./custom.css";
-import "./custom.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
